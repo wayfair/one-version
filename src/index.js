1d858169147193929800e3f920a27b958a64ed45
Seraphina Orsini <orsini.seraphina@gmail.com>
2022-05-02 12:48:34 -0400

    Fix cli command name (#14)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 #! /usr/bin/env node
-const {Command} = require('commander');
-const {version} = require('../package.json');
-const {check} = require('./check');
-const chalk = require('chalk');
+const { Command } = require("commander");
+const { version } = require("../package.json");
+const { check } = require("./check");
+const chalk = require("chalk");
 
 const program = new Command();
 
 program
-  .name('one-version-rule')
-  .usage('command [options]')
-  .version(version, '-v, --version');
+  .name("one-version")
+  .usage("command [options]")
+  .version(version, "-v, --version");
 
 program
-  .command('check')
+  .command("check")
   .description(
-    'Verify that only one version of each dependency exists in a monorepo'
+    "Verify that only one version of each dependency exists in a monorepo"
   )
-  .option('-p, --packageManager <type>', 'package manager', 'pnpm')
-  .action(({packageManager}) => {
+  .option("-p, --packageManager <type>", "package manager", "pnpm")
+  .action(({ packageManager }) => {
     try {
-      check({packageManager});
+      check({ packageManager });
     } catch (e) {
       console.log(chalk.red(e.message));
       process.exit(1);
