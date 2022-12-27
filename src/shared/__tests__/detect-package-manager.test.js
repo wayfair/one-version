8a87550b11a8c449cde5af4ef0cfe8f142f79770
Eric Jacobson <ejacobson@wayfair.com>
2022-12-27 08:56:12 -0500

    format and lint

--- src/shared/__tests__/detect-package-manager.test.js
+++ src/shared/__tests__/detect-package-manager.test.js
@@ -3,36 +3,36 @@ const { detectPackageManager } = require("../detect-package-manager");
 const fs = require("fs");
 
 jest.mock("fs", () => ({
-	...jest.requireActual("fs"),
-	existsSync: jest.fn(),
+  ...jest.requireActual("fs"),
+  existsSync: jest.fn(),
 }));
 
 describe("detectPackageManager", () => {
-	it("returns pnpm if pnpm-lock.yml exists", () => {
-		// first check for yarn.lock, then pnpm
-		fs.existsSync.mockReturnValueOnce(false).mockReturnValueOnce(true);
-		const packageManager = detectPackageManager();
-		expect(packageManager).toBe("pnpm");
-	});
+  it("returns pnpm if pnpm-lock.yml exists", () => {
+    // first check for yarn.lock, then pnpm
+    fs.existsSync.mockReturnValueOnce(false).mockReturnValueOnce(true);
+    const packageManager = detectPackageManager();
+    expect(packageManager).toBe("pnpm");
+  });
 
-	it("returns yarn if yarn.lock exists", () => {
-		// first check for yarn.lock, then yarnrc
-		fs.existsSync.mockReturnValueOnce(true).mockReturnValueOnce(false);
-		const packageManager = detectPackageManager();
-		expect(packageManager).toBe("yarn");
-	});
+  it("returns yarn if yarn.lock exists", () => {
+    // first check for yarn.lock, then yarnrc
+    fs.existsSync.mockReturnValueOnce(true).mockReturnValueOnce(false);
+    const packageManager = detectPackageManager();
+    expect(packageManager).toBe("yarn");
+  });
 
-	it("returns berry if yarn.lock and .yarnrc.yml exist", () => {
-		// first check for yarn.lock, then yarnrc
-		fs.existsSync.mockReturnValueOnce(true).mockReturnValueOnce(true);
-		const packageManager = detectPackageManager();
-		expect(packageManager).toBe("berry");
-	});
+  it("returns berry if yarn.lock and .yarnrc.yml exist", () => {
+    // first check for yarn.lock, then yarnrc
+    fs.existsSync.mockReturnValueOnce(true).mockReturnValueOnce(true);
+    const packageManager = detectPackageManager();
+    expect(packageManager).toBe("berry");
+  });
 
-	it("returns empty if package manager is not detected", () => {
-		// first check for yarn.lock, then pnpm
-		fs.existsSync.mockReturnValueOnce(false).mockReturnValueOnce(false);
-		const packageManager = detectPackageManager();
-		expect(packageManager).toBe("");
-	});
+  it("returns empty if package manager is not detected", () => {
+    // first check for yarn.lock, then pnpm
+    fs.existsSync.mockReturnValueOnce(false).mockReturnValueOnce(false);
+    const packageManager = detectPackageManager();
+    expect(packageManager).toBe("");
+  });
 });
